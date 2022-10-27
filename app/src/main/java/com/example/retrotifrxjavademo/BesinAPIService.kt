f093bec1d4b54643c7e2c8b5584fa20fa0eff7d8
Enes Ceviz <cevizenes01@gmail.com>
2022-10-27 08:33:25 +0300

    apply changes

--- app/src/main/java/com/example/retrotifrxjavademo/BesinAPIService.kt
+++ app/src/main/java/com/example/retrotifrxjavademo/BesinAPIService.kt
@@ -8,7 +8,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 class BesinAPIService {
     // // URL https://raw.githubusercontent.com/atilsamancioglu/BTK20-JSONVeriSeti/master/besinler.json
 
-    private val baseURL = "https://raw.githubusercontent.com"
+    private val baseURL = "https://raw.githubusercontent.com/"
     private val api = Retrofit.Builder()
             .baseUrl(baseURL)
             .addConverterFactory(GsonConverterFactory.create())
