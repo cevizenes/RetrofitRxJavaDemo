ce04134ac227c1c3fa61a7edbc446624d0daddf9
Enes Ceviz <cevizenes01@gmail.com>
2022-11-01 10:28:54 +0300

    rvAdapter updated

--- app/src/main/java/com/example/retrotifrxjavademo/rvAdapter.kt
+++ app/src/main/java/com/example/retrotifrxjavademo/rvAdapter.kt
@@ -6,27 +6,27 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.retrotifrxjavademo.databinding.RvBinding
 import kotlinx.android.synthetic.main.rv.view.*
 
 
 class rvAdapter(private val foodList: ArrayList<Food>): RecyclerView.Adapter<rvAdapter.Holder>() {
 
-    class Holder(view:View) : RecyclerView.ViewHolder(view){
+    class Holder(val binding: RvBinding) : RecyclerView.ViewHolder(binding.root){
 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
-        val binding = LayoutInflater.from(parent.context)
-        val view =binding.inflate(R.layout.rv,parent,false)
-        return Holder(view)
+        val binding = RvBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return Holder(binding)
     }
 
     override fun onBindViewHolder(holder: Holder, position: Int) {
-        holder.itemView.foodName.text = foodList.get(position).name
-        holder.itemView.calori.text = foodList.get(position).calori
-        holder.itemView.carbonhidrat.text = foodList.get(position).carbonhidrat
-        holder.itemView.protein.text = foodList.get(position).protein
-        holder.itemView.oil.text = foodList.get(position).oil
+        holder.binding.foodName.text = foodList.get(position).name
+        holder.binding.calori.text = foodList.get(position).calori
+        holder.binding.carbonhidrat.text = foodList.get(position).carbonhidrat
+        holder.binding.protein.text = foodList.get(position).protein
+        holder.binding.oil.text = foodList.get(position).oil
     }
 
     override fun getItemCount(): Int {
